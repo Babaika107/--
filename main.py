7206aee351f5a675ff57355bca37e406f2240c0d
German Leus <johndoe@email.com>
2024-11-14 10:36:31 +0200

    дз 1

---
+++ main.py
@@ -0,0 +1,14 @@
+#zadacha 1
+x = 1
+a = x
+y = 1
+b = y
+c = a*x + b*y
+print(c)
+#zadacha 2
+A = float(input('Уведыть масу цукерок '))
+X = float(input('Уведіть ціну 1 кг цукерок '))
+B = int(input('Уведіть кіькість пляшок лимонаду'))
+Y = float(input('Уведіть кількість пляшок лимонаду'))
+C = A*X + B*Y 
+print('Вартість покупки: ', round(c,2), 'грн')
